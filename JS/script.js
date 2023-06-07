f8d02e39c8d8b527aadcc7d69bba4a3d645b7bb3
quintusdev <quintusdev92@gmail.com>
2023-06-07 14:36:02 +0200

    ES. BASE COMPLETATO

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+//Salvo in una variabile globale la funzione createApp dall'istanza di Vue
+const { createApp } = Vue;
+
+//Utilizzo la funzione createApp
+createApp({
+    //Definisco il data con il return all'interno
+    data() {
+        return {
+            //All'interno del return definisco le variabili
+            saluto: '<h1>Ciao sono VUE JS!</h1>'
+        }
+    }
+    //Collego il createApp con l'elemento del DOM corrispondente
+}).mount('#app');
